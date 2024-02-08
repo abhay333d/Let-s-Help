c5985d73f88bb4b9cbd90992e164dec8a27bd398
abhay333d <abhaydandge333@gmail.com>
2024-02-08 17:09:04 +0530

    added about us page

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId)
-    
-    toggle.addEventListener('click', () => {
-        nav.classList.toggle('show-menu')
-        toggle.classList.toggle('show-icon')
-    })
-}
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
 
-showMenu('nav-toggle','nav-menu')
+  toggle.addEventListener("click", () => {
+    nav.classList.toggle("show-menu");
+    toggle.classList.toggle("show-icon");
+  });
+};
+
+showMenu("nav-toggle", "nav-menu");
+
+function AboutUsfunction() {
+  window.location.href = "about.html";
+}
